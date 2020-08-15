c41e21bc2cdecb464d8708371bcadcd751c3c2dc
William Terry <Tezza48is@gmail.com>
2020-08-15 22:21:28 +0100

    InputManager

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use bevy:: {
     prelude::*,
-    input::mouse::{ MouseButtonInput, MouseMotion },
+    input::{
+        mouse::{ MouseButtonInput, MouseMotion },
+        keyboard::{ KeyboardInput },
+    },
 };
 
 struct PlayerControl;
@@ -19,31 +22,34 @@ impl Default for FaceTowardsParent {
     }
 }
 
-// #[derive(Default)]
-// struct InputManager {
-//     movement: [f32; 2],
-//     look: [f32; 2],
-// }
+#[derive(Default)]
+struct InputManager {
+    movement: Vec2,
+    look: Vec2,
+    camera_zoom: f32,
+}
 
 #[derive(Default)]
 struct State {
-    mouse_button_event_reader: EventReader<MouseButtonInput>,
+    // mouse_button_event_reader: EventReader<MouseButtonInput>,
     mouse_motion_event_reader: EventReader<MouseMotion>,
-    cursor_moved_event_reader: EventReader<CursorMoved>,
+    // cursor_moved_event_reader: EventReader<CursorMoved>,
 }
 
 fn main() {
     App::build()
     .add_resource(Msaa { samples: 4 })
     .init_resource::<State>()
-    // .init_resource::<InputManager>()
+    .init_resource::<InputManager>()
     .add_default_plugins()
     .add_startup_system(setup.system())
-    .add_system(mouse_look.system())
+    .add_system(clear_input_manager.system())
+    .add_system(process_mouse_events.system())
+    .add_system(process_keys.system())
+    .add_system(rotate_player.system())
     .add_system(move_player.system())
-    // .add_system(rotate_player.system())
     .add_system(player_camera_target.system())
-    .add_system(camera_control.system())
+    .add_system(perform_camera_zoom.system())
     .run();
 }
 
@@ -86,51 +92,58 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
         });
 }
 
-// fn process_input_events(mut state: ResMut<State>, mut input: )
-
-fn rotate_player(time: Res<Time>, input: Res<Input<KeyCode>>, mut query: Query<(&PlayerControl, &mut Rotation)>) {
-    let mut rot = 0.0;
-    if input.pressed(KeyCode::Q) { rot += 1.0 }
-    if input.pressed(KeyCode::E) { rot -= 1.0 }
-
-    rot *= time.delta_seconds * 10.0;
-
-    for (_, mut rotation) in &mut query.iter() {
-
-        rotation.0 *= Quat::from_rotation_y(rot);
-    }
+fn clear_input_manager(mut input: ResMut<InputManager>) {
+    input.look = Vec2::zero(); // This one is event driven so needs to be cleared at the start of the frame.
 }
 
-fn mouse_look(
-    time: Res<Time>,
-    mut state: ResMut<State>,
-    mouse_button_input_events: Res<Events<MouseButtonInput>>,
-    mouse_motion_events: Res<Events<MouseMotion>>, 
-    mut cams: Query<&mut FaceTowardsParent>,
-    mut players: Query<(&PlayerControl, &mut Rotation)>
+fn process_mouse_events(
+    mut state: ResMut<State>, 
+    mut input: ResMut<InputManager>, 
+    mouse_motion_events: Res<Events<MouseMotion>>
 ) {
     for event in state.mouse_motion_event_reader.iter(&mouse_motion_events) {
-        for mut player in &mut cams.iter() {
-            player.angle = (player.angle - event.delta.y() * time.delta_seconds * 0.1).max(1f32.to_radians()).min(std::f32::consts::PI - 1f32.to_radians());
-            
-            println!("angle: {}", player.angle);
-        }
-
-        for (_, mut rotation) in &mut players.iter() {
-
-            rotation.0 *= Quat::from_rotation_y(-event.delta.x() * time.delta_seconds * 0.1);
-        }
+        input.look = event.delta;
     }
-
-    // for event in state.mouse_button_event_reader.iter(&mouse_button_input_events) {
-        
-    // }
 }
 
-fn camera_control(time: Res<Time>, input: Res<Input<KeyCode>>, mut cams: Query<&mut FaceTowardsParent>) {
-    let mut dist = 0.0;
-    if input.pressed(KeyCode::R) { dist += time.delta_seconds; }
-    if input.pressed(KeyCode::F) { dist -= time.delta_seconds; }
+fn process_keys(input: Res<Input<KeyCode>>, mut manager: ResMut<InputManager>) {
+    manager.movement = Vec2::zero();
+    if input.pressed(KeyCode::W) { *manager.movement.x_mut() += 1.; }
+    if input.pressed(KeyCode::S) { *manager.movement.x_mut() -= 1.; }
+    if input.pressed(KeyCode::D) { *manager.movement.y_mut() += 1.; }
+    if input.pressed(KeyCode::A) { *manager.movement.y_mut() -= 1.; }
+
+    if manager.movement != Vec2::zero() { manager.movement.normalize(); }
+
+
+    manager.camera_zoom = 0.;
+    if input.pressed(KeyCode::R) { manager.camera_zoom += 1.; }
+    if input.pressed(KeyCode::F) { manager.camera_zoom -= 1.; }
+}
+
+fn rotate_player(
+    time: Res<Time>,
+    input: Res<InputManager>,
+    mut query: Query<(&PlayerControl, &mut Rotation)>,
+    mut cams: Query<&mut FaceTowardsParent>,
+) {
+    let rot = (input.look * Vec2::new(2., 1.0)) * time.delta_seconds;
+
+    for (_, mut rotation) in &mut query.iter() {
+        rotation.0 *= Quat::from_rotation_y(-rot.x());
+    }
+
+    for mut cam in &mut cams.iter() {
+        cam.angle = (cam.angle - rot.y()).max(1f32.to_radians()).min(std::f32::consts::PI - 1f32.to_radians());
+    }
+}
+
+fn perform_camera_zoom(
+    time: Res<Time>, 
+    input: Res<InputManager>, 
+    mut cams: Query<&mut FaceTowardsParent>
+) {
+    let dist = input.camera_zoom * time.delta_seconds;
 
     for mut cam in &mut cams.iter() {
         cam.distance += dist * 5.0;
@@ -138,20 +151,18 @@ fn camera_control(time: Res<Time>, input: Res<Input<KeyCode>>, mut cams: Query<&
     }
 }
 
-fn move_player(time: Res<Time>, input: Res<Input<KeyCode>>, mut query: Query<(&PlayerControl, &Transform, &mut Translation)>) {
-    let mut horiz = 0.0;
-    let mut verti = 0.0;
-
-    if input.pressed(KeyCode::W) { verti += 1.0 }
-    if input.pressed(KeyCode::S) { verti -= 1.0 }
-    if input.pressed(KeyCode::D) { horiz += 1.0 }
-    if input.pressed(KeyCode::A) { horiz -= 1.0 }
+fn move_player(
+    time: Res<Time>, 
+    input: Res<InputManager>, 
+    mut query: Query<(&PlayerControl, &Transform, &mut Translation)>
+) {
+    let movement = input.movement * time.delta_seconds;
 
     for (_, transform, mut translation) in &mut query.iter() {
         let fwd = transform.value.z_axis().truncate();
         let right = -transform.value.x_axis().truncate();
 
-        let delta = fwd * verti + right * horiz;
+        let delta = fwd * movement.x() + right * movement.y();
         if delta == Vec3::zero() { continue; }
         let delta = delta.normalize() * time.delta_seconds * 10.0;
 
