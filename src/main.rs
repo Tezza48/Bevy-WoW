b0d8b1ff01fd88415959d825fde69acc83c28d63
William Terry <Tezza48is@gmail.com>
2020-08-18 18:23:41 +0100

    Way better ecs usage

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ use bevy:: {
     input::mouse::MouseMotion,
 };
 
-struct PlayerControl;
+struct MMOPlayer {
+    yaw: f32,
 
-struct FaceTowardsParent {
-    offset: Vec3,
-    distance: f32,
-    angle: f32,
+    camera_distance: f32,
+    camera_pitch: f32,
+    camera_entity: Option<Entity>,
 }
 
-impl Default for FaceTowardsParent {
+impl Default for MMOPlayer {
     fn default() -> Self {
-        FaceTowardsParent {
-            offset: Vec3::default(),
-            distance: 10.0,
-            angle: 30.0f32.to_radians(),
+        MMOPlayer {
+            yaw: 0.,
+
+            camera_distance: 20.,
+            camera_pitch: 30.0f32.to_radians(),
+            camera_entity: None,
         }
     }
 }
@@ -43,10 +45,10 @@ fn main() {
     .add_system(clear_input_manager.system())
     .add_system(process_mouse_events.system())
     .add_system(process_keys.system())
-    .add_system(rotate_player.system())
-    .add_system(move_player.system())
-    .add_system(player_camera_target.system())
-    .add_system(perform_camera_zoom.system())
+    .add_system(player_input.system())
+
+    // Actual system for updating the player and camera based on input.
+    .add_system(update_player.system())
     .run();
 }
 
@@ -57,21 +59,30 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
         cube_material
     });
 
-    commands
+    // Spawn camera and player, set entity for camera on player.
+    let camera_entity = commands
+        .spawn(Camera3dComponents::default())
+        .current_entity();
+
+    let player_entity = commands
         .spawn(PbrComponents {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
             material: cube_mat_handle.clone(),
             translation: Translation::new(0.0, 1.0, 0.0),
             ..Default::default()
         })
-        .with(PlayerControl)
-        .with(Rotation)
-        .with_children(|parent| {
-            parent
-                .spawn(Camera3dComponents::default())
-                .with(Rotation)
-                .with(FaceTowardsParent::default());
+        .with(MMOPlayer {
+            camera_entity,
+            camera_distance: 20.,
+            ..Default::default()
         })
+        .current_entity();
+
+    commands
+        // Append camera to player as child.
+        .push_children(player_entity.unwrap(), &[camera_entity.unwrap()])
+
+        // Create the environment.
         .spawn(PbrComponents {
             mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
             material: materials.add(Color::rgb(0.7, 0.3, 0.0).into()),
@@ -89,84 +100,71 @@ fn clear_input_manager(mut input: ResMut<InputManager>) {
 
 fn process_mouse_events(
     mut state: ResMut<State>, 
-    mut input: ResMut<InputManager>, 
+    mut input_manager: ResMut<InputManager>, 
     mouse_motion_events: Res<Events<MouseMotion>>
 ) {
     for event in state.mouse_motion_event_reader.iter(&mouse_motion_events) {
-        input.look = event.delta;
+        input_manager.look = event.delta;
     }
 }
 
 fn process_keys(input: Res<Input<KeyCode>>, mut manager: ResMut<InputManager>) {
     manager.movement = Vec2::zero();
-    if input.pressed(KeyCode::W) { *manager.movement.x_mut() += 1.; }
-    if input.pressed(KeyCode::S) { *manager.movement.x_mut() -= 1.; }
-    if input.pressed(KeyCode::D) { *manager.movement.y_mut() += 1.; }
-    if input.pressed(KeyCode::A) { *manager.movement.y_mut() -= 1.; }
+    if input.pressed(KeyCode::W) { *manager.movement.y_mut() += 1.; }
+    if input.pressed(KeyCode::S) { *manager.movement.y_mut() -= 1.; }
+    if input.pressed(KeyCode::D) { *manager.movement.x_mut() += 1.; }
+    if input.pressed(KeyCode::A) { *manager.movement.x_mut() -= 1.; }
 
     if manager.movement != Vec2::zero() { manager.movement.normalize(); }
-
 
     manager.camera_zoom = 0.;
     if input.pressed(KeyCode::R) { manager.camera_zoom += 1.; }
     if input.pressed(KeyCode::F) { manager.camera_zoom -= 1.; }
 }
 
-fn rotate_player(
+fn player_input(
     time: Res<Time>,
     input: Res<InputManager>,
-    mut query: Query<(&PlayerControl, &mut Rotation)>,
-    mut cams: Query<&mut FaceTowardsParent>,
+    mut query: Query<&mut MMOPlayer>,
 ) {
-    let rot = (input.look * Vec2::new(2., 1.0)) * time.delta_seconds;
-
-    for (_, mut rotation) in &mut query.iter() {
-        rotation.0 *= Quat::from_rotation_y(-rot.x());
-    }
-
-    for mut cam in &mut cams.iter() {
-        cam.angle = (cam.angle - rot.y()).max(1f32.to_radians()).min(std::f32::consts::PI - 1f32.to_radians());
-    }
-}
-
-fn perform_camera_zoom(
-    time: Res<Time>, 
-    input: Res<InputManager>, 
-    mut cams: Query<&mut FaceTowardsParent>
-) {
+    let rot = input.look * time.delta_seconds;
     let dist = input.camera_zoom * time.delta_seconds;
 
-    for mut cam in &mut cams.iter() {
-        cam.distance += dist * 5.0;
-        cam.distance = cam.distance.max(1.0).min(30.0);
+    for mut player in &mut query.iter() {
+        player.yaw += rot.x();
+        player.camera_pitch = (player.camera_pitch + rot.y()).max(1f32.to_radians()).min(179f32.to_radians());
+
+        player.camera_distance += dist * 5.0;
+        player.camera_distance = player.camera_distance.max(1.0).min(30.0);
     }
 }
 
-fn move_player(
+fn update_player(
     time: Res<Time>, 
     input: Res<InputManager>, 
-    mut query: Query<(&PlayerControl, &Transform, &mut Translation)>
+    mut player_query: Query<(&MMOPlayer, &Transform, &mut Translation, &mut Rotation)>,
+    cameras: Query<(&mut Translation, &mut Rotation)>
 ) {
-    let movement = input.movement * time.delta_seconds;
+    for (player, transform, mut translation, mut rotation) in &mut player_query.iter() {
+        let fwd = transform.value.z_axis().truncate() * input.movement.y();
+        let right = transform.value.x_axis().truncate() * input.movement.x();
+    
+        // TODO WT: Either normalize the direction or normalize the input.movement outside (latter is better for performance here);
+    
+        translation.0 += Vec3::from(fwd + right) * time.delta_seconds;
 
-    for (_, transform, mut translation) in &mut query.iter() {
-        let fwd = transform.value.z_axis().truncate();
-        let right = -transform.value.x_axis().truncate();
+        rotation.0 = Quat::from_rotation_y(-player.yaw);
 
-        let delta: Vec3 = (fwd * movement.x() + right * movement.y()).into();
-        if delta == Vec3::zero() { continue; }
-        let delta = delta.normalize() * time.delta_seconds * 10.0;
+        if let Some(camera_entity) = player.camera_entity {
+            let cam_pos = Vec3::new(0., player.camera_pitch.cos(), -player.camera_pitch.sin()).normalize() * player.camera_distance;
+            if let Ok(mut cam_trans) = cameras.get_mut::<Translation>(camera_entity) {
+                cam_trans.0 = cam_pos;
+            }
 
-        translation.0 += delta;
-    }
-}
-
-fn player_camera_target(mut look: Query<(&FaceTowardsParent, &mut Translation, &mut Rotation)>) {
-    for (face_towards, mut translation, mut rotation) in &mut look.iter() {
-        let to = Vec3::new(0., face_towards.angle.cos(), -face_towards.angle.sin()).normalize();
-        translation.0 = to * face_towards.distance + face_towards.offset;
-
-        let look = Mat4::face_toward(translation.0, Vec3::zero(), Vec3::new(0.0, 1.0, 0.0));
-        rotation.0 = look.to_scale_rotation_translation().1;
+            if let Ok(mut camera_rotation) = cameras.get_mut::<Rotation>(camera_entity) {
+                let look = Mat4::face_toward(cam_pos, Vec3::zero(), Vec3::new(0.0, 1.0, 0.0));
+                camera_rotation.0 = look.to_scale_rotation_translation().1;
+            }
+        }
     }
 }
